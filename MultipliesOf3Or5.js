16abd66305e80c9364424e7b02b280fc59ad9679
maneletorres <maneletorres@hotmail.com>
2018-12-24 23:08:06 +0100

    The script 'MultipliesOf3Or5' has been documented by comment.

--- MultipliesOf3Or5.js
+++ MultipliesOf3Or5.js
@@ -1,3 +1,21 @@
+/**
+ * 
+ * @author Manuel Espinosa Torres
+ * 
+ * Codewars exercise: "Multiplies of 3 or 5"
+ * 
+ * Description:
+ * Finish the solution so that it returns the sum of all the multiples
+ * of 3 or 5 below the number passed in.
+ * 
+ * Example:
+ * If we list all the natural numbers below 10 that are multiples of 3
+ * or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * 
+ * Note:
+ * If the number is a multiple of both 3 and 5, only count it once.
+ * 
+ */
 const readline = require('readline');
 
 const rl = readline.createInterface({
