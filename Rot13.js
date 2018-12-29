ee8533d579d07a9fa7c6b34cd0d28e0df1b6b1d3
maneletorres <maneletorres@hotmail.com>
2018-12-29 08:03:28 +0100

    Added the solution in JavaScript for Codewars exercise 'Rot13'.

---
+++ Rot13.js
@@ -0,0 +1,46 @@
+/**
+ * 
+ * @author Manuel Espinosa Torres
+ * 
+ * Codewars exercise: "Rot13"
+ * 
+ * Description:
+ * ROT13 is a simple letter substitution cipher that replaces
+ * a letter with the letter 13 letters after it in the alphabet.
+ * ROT13 is an example of the Caesar cipher.
+ * 
+ * Create a function that takes a string and returns the string
+ * ciphered with Rot13. If there are numbers or special 
+ * characters included in the string, they should be returned 
+ * as they are. Only letters from the latin/english alphabet 
+ * should be shifted, like in the original Rot13 "implementation".
+ * 
+ */
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function rot13(message){
+    var result = "";
+
+    for(var i = 0; i < message.length; i++){
+        var asciiCode = message.charCodeAt(i);
+        if((asciiCode >= 110 && asciiCode <= 122) || (asciiCode >= 78 && asciiCode <= 90)){
+            result += String.fromCharCode(message.charCodeAt(i) - 13);
+        } else if((asciiCode >= 97 && asciiCode <= 109) || (asciiCode >= 65 && asciiCode <= 97)){
+            result += String.fromCharCode(message.charCodeAt(i) + 13);
+        } else {
+            result += String.fromCharCode(message.charCodeAt(i));
+        }
+    }
+
+    return result;
+}
+
+rl.question('Enter the string you want to encrypt: ', (userInput) => {
+    console.log('The result of encrypting the string "' + userInput + '" with ROT13 encryption is: ' + rot13(userInput));
+    rl.close();
+});
