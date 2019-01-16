417be48de4a0626a25e3ef8356754a9eee90fdb0
maneletorres <maneletorres@hotmail.com>
2019-01-16 22:07:21 +0100

    Adapted the script 'EvenOrOdd.js' to the new approach.

--- EvenOrOdd.js
+++ EvenOrOdd.js
@@ -5,11 +5,11 @@
  * Codewars exercise: "Even or Odd"
  * 
  * Description:
- * Create a function (or write a script in Shell) that takes an integer
- * as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+ * Create a function (or write a script in Shell) that takes an integer as an  * argument and returns "Even" for even numbers or "Odd" for odd numbers.
  * 
  */
-const readline = require('readline');
+// Individual script:
+/*const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -23,4 +23,16 @@ function even_or_odd(number) {
 rl.question('Enter a number to start the "Even or Odd" function: ', (userInput) => {
   console.log("The number " + userInput + " is: " + even_or_odd(userInput) + ".");
   rl.close();
-});
+});*/
+
+/**
+ * Alternative approach:
+ * 
+ * Use of index for the export of the various functions of 
+ * Codewars exercise.
+ */
+module.exports = { 
+  even_or_odd: function even_or_odd(number){
+    return "The number " + number + " is " + (number % 2 == 0 ? "even." : "ddd.");
+  }
+};
